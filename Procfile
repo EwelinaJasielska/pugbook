9d884eb15b9b6961fc65e78adf66b949ecfefde9
EwelinaJasielska <donnie20555@gmail.com>
2021-06-09 01:39:26 +0200

    pug10 heroku
    pug 10 heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn backend.wsgi --log-file -
+web: gunicorn pug10.wsgi
---
+++ pug10app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Pug10AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pug10app'
